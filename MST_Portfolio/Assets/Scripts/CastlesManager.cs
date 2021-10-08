4b6c0f7a283b346cfe52aade4cd0df3d46da9a3f
serhattatar <mserhattatar@gmail.com>
2021-10-08 22:16:08 +0300

    enemy and player power fixed

--- MST_Portfolio/Assets/Scripts/CastlesManager.cs
+++ MST_Portfolio/Assets/Scripts/CastlesManager.cs
@@ -6,13 +6,13 @@ public class CastlesManager : MonoBehaviour
     private ComponentContainer MyComponent;
 
     private SpawnManager _spawnManager;
-    
+
     [SerializeField] private ParticleSystem enemyParticle;
     [SerializeField] private ParticleSystem enemyAddPowerIconParticle;
     [SerializeField] private ParticleSystem playerParticle;
     [SerializeField] private ParticleSystem playerAddPowerIconParticle;
-    
-    private const float MaxPower = 26;
+
+    private const float MaxPower = 50;
     private float _enemyPower;
     private float _playerPower;
     public int levelPower;
@@ -21,46 +21,57 @@ public class CastlesManager : MonoBehaviour
     {
         MyComponent = componentContainer;
     }
+
     private void Start()
     {
         _spawnManager = MyComponent.GetComponent("SpawnManager") as SpawnManager;
     }
 
-    public void AddPower(bool isPlayer, int addPower)
+    public void AddPower(bool isPlayer, float addPower, bool checkHit = true)
     {
         if (isPlayer)
         {
             _playerPower += addPower;
-            playerAddPowerIconParticle.Play();
+            if (checkHit)
+                playerAddPowerIconParticle.Play();
+            var playerParticleMain = playerParticle.main;
+            playerParticleMain.startSpeed = _playerPower / levelPower;
         }
         else
         {
             _enemyPower += addPower;
-            enemyAddPowerIconParticle.Play();
+            if (checkHit)
+                enemyAddPowerIconParticle.Play();
+            var enemyParticleMain = enemyParticle.main;
+            enemyParticleMain.startSpeed = _enemyPower / levelPower;
         }
-        _spawnManager.GeneratePowerIcon(addPower);
-           
-        ChangePower();
-    }
 
-    private void ChangePower()
-    {
-        var enemyParticleMain = enemyParticle.main;
-        enemyParticleMain.startSpeed = _enemyPower / levelPower;
+        if (checkHit && (_enemyPower / levelPower) + (_playerPower / levelPower) >= MaxPower)
+        {
+            var diff = (((_enemyPower / levelPower) + (_playerPower / levelPower)) - MaxPower);
+            AddPower(!isPlayer, -diff, false);
+        }
 
-        var playerParticleMain = playerParticle.main;
-        playerParticleMain.startSpeed = _playerPower / levelPower;
+
+        _spawnManager.GeneratePowerIcon((int)addPower);
+
 
         IsThereAWinner();
     }
+
 
     private void IsThereAWinner()
     {
         if (_enemyPower / levelPower >= MaxPower)
         {
+            Debug.Log("Enemy Kazandı");
+            Time.timeScale = 0f;
         }
         else if (_playerPower / levelPower >= MaxPower)
         {
+            Debug.Log("Enemy Kazandı");
+
+            Time.timeScale = 0f;
         }
     }
 }
