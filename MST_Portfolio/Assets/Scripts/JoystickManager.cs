df573088f535ef51ec6f7dd4b1498543acc85a89
serhattatar <mserhattatar@gmail.com>
2021-11-12 10:46:48 +0300

    unity updated, joystick test

--- MST_Portfolio/Assets/Scripts/JoystickManager.cs
+++ MST_Portfolio/Assets/Scripts/JoystickManager.cs
@@ -15,11 +15,11 @@ public class JoystickManager : MonoBehaviour
     {
         JoystickHorizontal = _joystick.Horizontal;
 
-        if (_joystick.Vertical <= 0.0f)
-        {
-            JoystickVertical = 0;
-            return;
-        }
+        //if (_joystick.Vertical <= 0.0f)
+        //{
+        //    JoystickVertical = 0;
+        //    return;
+        //}
         JoystickVertical = _joystick.Vertical;
     }
 }
